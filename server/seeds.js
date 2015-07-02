95c725deed520fb5cd81e77dbc8bb18c24c25c98
jvalentino90 <jvalentino90@gmail.com>
2015-07-02 01:37:08 -0300

    Primeros 2 tests pasados

--- server/seeds.js
+++ server/seeds.js
@@ -1,8 +1,10 @@
 
 Seed = { subjects: function() { 
-	Subjects.insert({ name: "Matematica"})
-	Subjects.insert({ name: "Fisica"})
- 	Subjects.insert({ name: "Lengua"})
-	}
+	if(Subjects.find().count() <1){
+		Subjects.insert({ name: "Matematica"})
+		Subjects.insert({ name: "Fisica"})
+ 		Subjects.insert({ name: "Lengua"})
+ 	}
+  }
 }
 
